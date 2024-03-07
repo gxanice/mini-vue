479ebfbdc672603e4c5bf5028465fc4491abd3e3
moon <1366741839@qq.com>
2024-03-07 10:54:36 +0800

    first commit

---
+++ src/reactivity/test/effect.spec.ts
@@ -0,0 +1,97 @@
+import { reactive } from "../reactive";
+import { effect, stop } from "../effect";
+
+describe("effect", () => {
+  it("happy path", () => {
+    const user = reactive({
+      age: 10,
+    });
+
+    let nextAge;
+    effect(() => {
+      nextAge = user.age + 1;
+    });
+
+    // 断言
+    expect(nextAge).toBe(11);
+
+    // 更新操作
+    user.age++;
+    expect(nextAge).toBe(12);
+  });
+
+  it("should return runner when call effect", () => {
+    // 1.effect(fn) -> function(runner) -> fn -> return
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return "foo";
+    });
+
+    expect(foo).toBe(11);
+    const r = runner();
+    expect(foo).toBe(12);
+    expect(r).toBe("foo");
+  });
+
+  it("scheduler", () => {
+    // 1.通过effect得第二个参数给定的一个scheduler函数
+    // 2.effect第一次执行时 会执行fn
+    // 3.当响应式对象更新时不会执行fn而是会执行scheduler函数
+    // 4.如果当执行runner时，会再次执行fn
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler }
+    );
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    // 应该执行第一个函数
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    expect(dummy).toBe(1);
+    // 应该执行第二个函数
+    run();
+    expect(dummy).toBe(2);
+  });
+
+  it("stop", () => {
+    let dummy;
+    const obj = reactive({ prop: 1 });
+    const runner = effect(() => {
+      dummy = obj.prop;
+    });
+    obj.prop = 2;
+    expect(dummy).toBe(2);
+    stop(runner);
+    obj.prop = 3;
+    expect(dummy).toBe(2);
+
+    runner();
+    expect(dummy).toBe(3);
+  });
+
+  it("onStop", () => {
+    const obj = reactive({ foo: 1 });
+    const onStop = jest.fn();
+    let dummy;
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      {
+        onStop,
+      }
+    );
+
+    stop(runner);
+    expect(onStop).toBeCalledTimes(1);
+  });
+});
