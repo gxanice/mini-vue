bc311c12f3154bc23fc83c525d061c1c446c221c
moon <1366741839@qq.com>
2024-03-20 22:27:54 +0800

    fix:修复应该被移动的节点变成先删除再添加，从而造成性能浪费的bug

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -233,7 +233,7 @@ export function createRenderer(options: any) {
         if (prevChild.key !== null) {
           newIndex = keyToNewIndexMap.get(prevChild.key);
         } else {
-          for (let j = s2; j < e2; j++) {
+          for (let j = s2; j <= e2; j++) {
             if (isSomeVnodeType(prevChild, c2[j])) {
               newIndex = j;
               break;
