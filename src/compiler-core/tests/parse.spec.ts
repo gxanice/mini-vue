d13680b054796c490acdefce75fa7257cee53eb1
moon <1366741839@qq.com>
2024-03-22 19:30:08 +0800

    feat:实现解析插值的功能

---
+++ src/compiler-core/tests/parse.spec.ts
@@ -0,0 +1,18 @@
+import { baseParse } from "../src/parse";
+
+describe("Parse", () => {
+  describe("interpolation", () => {
+    test("simple expression", () => {
+      const ast = baseParse("{{message}}");
+
+      // root
+      expect(ast.children[0]).toStrictEqual({
+        type: "interpolation",
+        content: {
+          type: "simple_expression",
+          content: "message",
+        },
+      });
+    });
+  });
+});
