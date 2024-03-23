b0089a449276a91fc949cbd5f32145f367f0e96c
moon <1366741839@qq.com>
2024-03-23 22:32:54 +0800

    feat:实现transform功能

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,36 @@
+import { NodeTypes } from "./ast";
+
+export function transform(root: any, options?: any) {
+  // 创建上下文对象
+  const context = createTransformContext(root, options);
+  // 深度优先搜索dps
+  traverseNode(root, context);
+  // 修改 text content
+}
+
+function createTransformContext(root: any, options: any) {
+  const context = {
+    root: root,
+    nodeTransforms: options.nodeTransforms || [],
+  };
+  return context;
+}
+
+function traverseNode(node: any, context: any) {
+  const nodeTransforms = context.nodeTransforms;
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i];
+    transform(node);
+  }
+  traverseChildren(node, context);
+}
+
+function traverseChildren(node: any, context: any) {
+  const children = node.children;
+
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      traverseNode(children[i], context);
+    }
+  }
+}
